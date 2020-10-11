267fde7a41ba7e64d57914fa963aab5e0f1470f8
Anya Lozovaya <anyalozovaya2014@gmail.com>
2020-10-12 01:37:57 +0400

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let valueCheck = true
+    let arrayStr = str.split('')
+    for (j = 0; j < bracketsConfig.length; j++) {
+        let itr = 0
+        while (itr < arrayStr.length) {
+            if (arrayStr[itr] === bracketsConfig[j][0]) {
+                if (arrayStr[itr + 1] === bracketsConfig[j][1]) {
+                    arrayStr.splice(itr, 2)
+                    if (arrayStr.length !== 0) {
+                        itr = 0
+                        j = 0
+                    } else if (itr === arrayStr.length - 1) itr = arrayStr.length + 1
+                } else {
+                    itr += 1
+                }
+            } else {
+                itr += 1
+            }
+        }
+
+    }
+    if (arrayStr.length !== 0) {
+        valueCheck = false
+    }
+    return valueCheck
 }
